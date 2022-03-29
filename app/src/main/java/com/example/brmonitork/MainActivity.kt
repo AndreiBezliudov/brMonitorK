31f996277bf60a5aaf5d7c5b1245420d2142a975
Andrei_Bezliudov <A.Bezliudov@gmail.com>
2022-03-29 23:02:50 +0300

    Initial commit

---
+++ app/src/main/java/com/example/brmonitork/MainActivity.kt
@@ -0,0 +1,34 @@
+package com.example.brmonitork
+
+import android.annotation.SuppressLint
+import android.bluetooth.BluetoothAdapter
+import android.bluetooth.BluetoothDevice
+import android.bluetooth.BluetoothManager
+import android.content.Context
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+
+class MainActivity : AppCompatActivity() {
+    private var btAdapter: BluetoothAdapter? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        init()
+    }
+
+    private fun init(){
+        val btManager = getSystemService(Context.BLUETOOTH_SERVICE) as BluetoothManager
+        btAdapter = btManager.adapter
+        getPairedDevices()
+    }
+
+    @SuppressLint("MissingPermission")
+    private fun getPairedDevices(){
+        val pairedDevices: Set<BluetoothDevice>? = btAdapter?.bondedDevices
+        pairedDevices?.forEach {
+            Log.d("MyLog", "Name: ${it.name}")
+        }
+    }
+}
